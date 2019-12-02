8564f33f8fd4a2e47251290dd1e47b1d1ac569a6
alvarofloresPM <alvaro.flores@processmaker.com>
2019-12-02 10:24:39 -0400

    port

--- SslScript.sh
+++ SslScript.sh
@@ -1,6 +1,10 @@
 mv /opt/sslcert/template.conf /opt/sslcert/$2.processmaker.net
 sed -i "/server_name template.processmaker.net;/c\server_name $2.processmaker.net;" /opt/sslcert/$2.processmaker.net
-sed -i "/proxy_pass              http:\/\/10.100.100.100;/c\proxy_pass              http:\/\/$1;" /opt/sslcert/$2.processmaker.net
+if [[ -z "$3" ]];
+then
+    sed -i "/proxy_pass              http:\/\/10.100.100.100;/c\proxy_pass              http:\/\/$1;" /opt/sslcert/$2.processmaker.net
+else
+    sed -i "/proxy_pass              http:\/\/10.100.100.100;/c\proxy_pass              http:\/\/$1:$3;" /opt/sslcert/$2.processmaker.net
 mv /opt/sslcert/$2.processmaker.net /etc/nginx/conf.d/
 if ! nginx -t ; then exit 1 ; fi
 if ! nginx -s reload ; then exit 1 ; fi
